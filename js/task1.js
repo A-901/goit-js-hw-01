dbb9b811791f4ce40937d8cd9ed3d1fcef226d17
Alexandr <e7000@ukr.net>
2019-12-17 17:39:52 +0200

    hw1

--- js/task1.js
+++
@@ -1,7 +0,0 @@
-'user strict'
-const name = 'Генератор защитного поля';
-let price = 1000;
-console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
-
-price = 2000;
-console.log(`Выбран «${name}», цена за штуку ${price} кредитов`);
--- js/task2.js
+++
@@ -1,9 +0,0 @@
-'user strict'
-const total = 100;
-const ordered = 50;
-
-if (total >= ordered) {
-  console.log('Заказ оформлен, с вами свяжется менеджер');
-} else {
-  console.log('На складе недостаточно твоаров!');
-}
